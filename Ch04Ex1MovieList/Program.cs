bafee22771a10e3d8ceb9cb238af522af2f4860f
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2024-10-06 15:24:45 -0500

    Step 12

--- Ch04Ex1MovieList/Program.cs
+++ Ch04Ex1MovieList/Program.cs
@@ -1,8 +1,17 @@
+using Ch04Ex1MovieList.Models;
+using Microsoft.EntityFrameworkCore;
+using Ch04Ex1MovieList.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+
+// Add services to the container. builder.Services.AddControllersWithViews(); // Add EF Core DI
+builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(
+builder.Configuration.GetConnectionString("MovieContext")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
